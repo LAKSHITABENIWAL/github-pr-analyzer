6a6a69d21f567ebb5991c5161945dbc13fade8dd
LAKSHITABENIWAL <91519034+LAKSHITABENIWAL@users.noreply.github.com>
2025-05-14 22:56:31 +0530

    update to fix the eror

--- backend/index.js
+++ backend/index.js
@@ -17,6 +17,7 @@ app.use(
   cors({
     origin: [
       'http://localhost:5173',
+      'http://localhost:3001',
       'https://lakshitabeniwal.github.io'  // Your specific GitHub Pages URL
     ],
     credentials: true,
